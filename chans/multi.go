217ae4283e24da2ceb5e0ca8217e4af3c311f9bb
Kushuh <kuzanisu@gmail.com>
2025-05-21 17:39:15 +0200

    chores: migrate golangci to v2

--- chans/multi.go
+++ chans/multi.go
@@ -4,8 +4,12 @@ import (
 	"sync"
 )
 
+// MultiChan forwards the data of a single channel to multiple listeners.
 type MultiChan[T any] struct {
-	src       chan T
+	// The source channel
+	src chan T
+	// The listeners that are registered to receive data from the source channel. The boolean key is used to indicate
+	// whether the listener is still active or not.
 	listeners map[chan T]bool
 
 	mu sync.Mutex
@@ -16,6 +20,7 @@ func (multi *MultiChan[T]) readMsg(msg T) {
 	defer multi.mu.Unlock()
 
 	for listener, ok := range multi.listeners {
+		// Listener is not active anymore.
 		if !ok {
 			continue
 		}
@@ -25,29 +30,34 @@ func (multi *MultiChan[T]) readMsg(msg T) {
 }
 
 func (multi *MultiChan[T]) listen() {
+	// Forward each new message to all listeners.
 	for msg := range multi.src {
 		multi.readMsg(msg)
 	}
 }
 
+// Chan returns the source channel of the MultiChan.
 func (multi *MultiChan[T]) Chan() chan<- T {
 	return multi.src
 }
 
+// Send a new message to the source channel. This will be forwarded to all listeners.
 func (multi *MultiChan[T]) Send(data T) {
 	multi.src <- data
 }
 
+// Register a new listener, and return it. The listener will receive all messages sent to the source channel.
 func (multi *MultiChan[T]) Register() <-chan T {
 	multi.mu.Lock()
 	defer multi.mu.Unlock()
 
 	listener := make(chan T)
-	multi.listeners[listener] = true
+	multi.listeners[listener] = true // mark active.
 
 	return listener
 }
 
+// Unregister a listener. The listener will no longer receive messages from the source channel.
 func (multi *MultiChan[T]) Unregister(src <-chan T) {
 	multi.mu.Lock()
 	defer multi.mu.Unlock()
@@ -60,6 +70,7 @@ func (multi *MultiChan[T]) Unregister(src <-chan T) {
 	}
 }
 
+// Close the source channel, along with all listeners.
 func (multi *MultiChan[T]) Close() {
 	close(multi.src)
 
